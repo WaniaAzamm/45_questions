ea543db99a7e92f98267ee4ce664127d140f6fe4
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-05 21:05:17 +0500

    Add files via upload

---
+++ 45_questions/question12/main.ts
@@ -0,0 +1,4 @@
+let names :string[] =["Anosha", "Alisha", "Areeba"];
+console.log(`Hello ${names[0]}, how are you?`);
+console.log(`Hello ${names[1]}, how are you?`);
+console.log(`Hello ${names[2]}, how are you?`);
