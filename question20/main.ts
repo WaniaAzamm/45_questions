87af98b5850dfe0a4dafa453950ac9da6febd43d
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-06 01:11:49 +0500

    Delete question20 directory

--- question20/main.ts
+++
@@ -1,8 +0,0 @@
-let mountainsList :string[] =["Annapurna", "Mount Everest","Manaslu", "K2", "Makalu", "Aragats"];
-console.log("List Of Mountains:");
-console.log(mountainsList[0]);
-console.log(mountainsList[1]);
-console.log(mountainsList[2]);
-console.log(mountainsList[3]);
-console.log(mountainsList[4]);
-console.log(mountainsList[5]);
