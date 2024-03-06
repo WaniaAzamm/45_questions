c184e3b535d8046f03ddb607e4aa87ad17e0cace
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-07 02:38:12 +0500

    Add files via upload

---
+++ 45_questions/question30/main.ts
@@ -0,0 +1,8 @@
+ let usernames :string[] =["Admin", "Wania", "Anosha","Anabia", "Areeba","Alisha"];
+ for(let username of usernames){
+    if(username == "Admin"){
+        console.log("Hello Admin, would you like to see a status report?")
+    }else{
+        console.log(`Hello ${username}, thank you for logging in again.`)
+    }
+ };
