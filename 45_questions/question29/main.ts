6b69e009d4dc4ffe8f58632fd8eb77e3ad20ad72
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-07 01:56:00 +0500

    Add files via upload

---
+++ 45_questions/question29/main.ts
@@ -0,0 +1,16 @@
+let favorite_fruits :string[] =["Orange","Grapes","Pomegranate"];
+if(favorite_fruits.includes("Orange")){
+    console.log("You really like oranges!")
+}
+if(favorite_fruits.includes("Grapes")){
+    console.log("You really like Grapes!")
+};
+if(favorite_fruits.includes("Pomegranate")){
+    console.log("You really like Pomegranate!")
+};
+if(favorite_fruits.includes("Berry")){
+    console.log("You really like Berry!")
+};
+if(favorite_fruits.includes("Strawberry")){
+    console.log("You really like Strawberry!")
+};
