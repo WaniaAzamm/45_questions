df19f368249f51f932b40f0d85bf95f22eeaeb36
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-07 01:06:26 +0500

    Add files via upload

---
+++ 45_questions/question26/main.ts
@@ -0,0 +1,15 @@
+//if block version:
+let alien_color :string ="Green";
+if(alien_color == "Green"){
+    console.log("Wow, You earn 5 points.")
+}else{
+    console.log("Wow, you earn 10 points.")
+};
+
+//else block version:
+let alien_color1 :string ="Red";
+if(alien_color1 == "Green"){
+    console.log("Wow, You earn 5 points.")
+}else{
+    console.log("Wow, you earn 10 points.")
+};
