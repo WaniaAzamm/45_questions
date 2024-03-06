869558ffa0bf2bf16e4a994f3ebc7d3bdfb0c1fc
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-07 01:16:11 +0500

    Add files via upload

---
+++ 45_questions/question27/main.ts
@@ -0,0 +1,27 @@
+//for green:
+let alien_color :string ="Green";
+if(alien_color == "Green"){
+    console.log("Wow, you earn 5 points")
+}else if(alien_color == "yellow"){
+    console.log("Wow, you earn 10 points")
+}else if(alien_color =="Red"){
+    console.log("Wow, you earn 15 points")
+};    
+//for yellow:
+let alien_color1 :string ="yellow";
+if(alien_color1 == "Green"){
+    console.log("Wow, you earn 5 points")
+}else if(alien_color1 == "yellow"){
+    console.log("Wow, you earn 10 points")
+}else if(alien_color1 =="Red"){
+    console.log("Wow, you earn 15 points")
+};    
+//for red:
+let alien_color2 :string ="Red";
+if(alien_color2 == "Green"){
+    console.log("Wow, you earn 5 points")
+}else if(alien_color2 == "yellow"){
+    console.log("Wow, you earn 10 points")
+}else if(alien_color2 =="Red"){
+    console.log("Wow, you earn 15 points")
+};    
