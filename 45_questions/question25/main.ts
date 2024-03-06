e3890bb47ecc7ac1695e790ea7da8f6cd64d702b
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-07 00:55:03 +0500

    Add files via upload

---
+++ 45_questions/question25/main.ts
@@ -0,0 +1,9 @@
+//pass condition:
+let alien_color : string ="Green";
+if(alien_color == "Green"){
+  console.log("WOW, you earn a 5 points.")
+};
+//fail condition:
+let alien_color1 : string ="Red";
+if(alien_color1 == "Green"){
+};
