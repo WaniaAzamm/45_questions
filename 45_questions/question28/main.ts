d5e7b1701c30357d382aac5682c54e767540fcf7
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-07 01:31:13 +0500

    Add files via upload

---
+++ 45_questions/question28/main.ts
@@ -0,0 +1,14 @@
+let Age :number =16;
+if(Age <2){
+    console.log("Person is a baby.")
+}else if(Age < 4){
+    console.log("Person is a toddler.")
+}else if(Age < 13){
+    console.log("Person is a kid.")
+}else if(Age < 20){
+    console.log("Person is a teenager.")
+}else if(Age < 65){
+    console.log("Person is an adult.")
+}else{
+    console.log("Person is an elder")
+};
