703e383847c4b117e96789e076e2d747bfd71cba
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-07 16:23:34 +0500

    Add files via upload

---
+++ 45_questions/question34/main.ts
@@ -0,0 +1,9 @@
+let favouritepizzas :string[] =["Cheese Pizza", "Tikka Pizza", "Malai Chicken Pizza"];
+for(let favouritepizza of favouritepizzas){
+    console.log(favouritepizza);
+}
+for(let favouritepizza of favouritepizzas){
+    console.log(`I really like ${favouritepizza}.`)
+}
+console.log("I really love pizza.");
+
