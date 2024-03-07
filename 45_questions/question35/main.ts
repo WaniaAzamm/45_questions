dc1c67ad3a2a8aafe04a164aa6c3b1c8b33aee98
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-07 16:35:50 +0500

    Add files via upload

---
+++ 45_questions/question35/main.ts
@@ -0,0 +1,8 @@
+let animals :string[] =["Dog","Cat","Rabbit"];
+for(let animal of animals){
+    console.log(animal)
+}
+for(let animal of animals){
+    console.log(`${animal} would make a great pet.`);
+}
+console.log("Any of these would make a great pet.")
