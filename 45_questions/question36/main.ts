061d3b259abd339df278926d839ae8b898afd156
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-07 22:25:53 +0500

    Add files via upload

---
+++ 45_questions/question36/main.ts
@@ -0,0 +1,4 @@
+function makeShirt(size: string, message:string ): void{ 
+    console.log(`Creating a ${size} sized Shirt with the message ${message}.`)
+};
+makeShirt("Large","Typescript Explorer");
