c95de1dde0df999402412a8b2d40326b4a6cc680
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-07 14:57:53 +0500

    Add files via upload

---
+++ 45_questions/question31/main.ts
@@ -0,0 +1,13 @@
+let usernames: string[] = ["Wania", "Anosha","Anabia", "Areeba","Alisha"];
+if (usernames.length > 0){
+    console.log("List of Usernames:",usernames)
+}else{
+    console.log("We need to find some users")
+};
+usernames =[];
+if (usernames.length > 0){
+    console.log("List of Usernames:",usernames)
+}else{
+    console.log("We need to find some users.")
+};
+
