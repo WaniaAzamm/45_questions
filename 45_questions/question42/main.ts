0da9a134c5b1a4ab4f8fb14cf44cc90a1410b7c5
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-08 15:30:02 +0500

    Add files via upload

---
+++ 45_questions/question42/main.ts
@@ -0,0 +1,14 @@
+let magicans :string[]=["Harry Houdini", "David Blaine", "Doug Henning","Dai Vernon"];
+function show_magicans(magicansArray:string[]):void{
+    console.log("Magicans:")
+    magicansArray.forEach(magicans =>{
+        console.log(magicans)
+    });
+}
+function make_great(magicansArray:string[]): string[]{
+    return magicansArray.map(magicans => `Great ${magicans}`)
+};
+let greatMagicans =make_great(magicans);
+show_magicans(greatMagicans);
+
+
