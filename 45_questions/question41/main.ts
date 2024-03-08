57b2071e86bc697641d72603b9649c3b660c9e70
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-08 15:22:55 +0500

    Add files via upload

---
+++ 45_questions/question41/main.ts
@@ -0,0 +1,8 @@
+let magicans :string[]=["Harry Houdini", "David Blaine", "Doug Henning","Dai Vernon"];
+function show_magicans(magicansArray:string[]):void{
+    console.log("Magicans:")
+    magicansArray.forEach(magicans =>{
+        console.log(magicans)
+    });
+}
+show_magicans(magicans);
