d38c42a75f78fc698dc5c9dd9173598588b88809
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-05-01 00:11:18 +0500

    main.ts

--- 45_questions/question37/main.ts
+++ 45_questions/question37/main.ts
@@ -1,4 +1,5 @@
-function makeShirt(size:string="Large",message:string="Typescript is Fantastic"):void{
+function makeShirt(size:string="Large",message:string="Typescript is Fantastic")
+{
     console.log(`Creating a ${size} sized shirt with the message ${message}.`)
 };
 makeShirt();
