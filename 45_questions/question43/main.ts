2cc09f63a5fff03f49452218351b0bc3c955779c
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-04-30 23:46:56 +0500

    main.ts

--- 45_questions/question43/main.ts
+++ 45_questions/question43/main.ts
@@ -1,14 +1,17 @@
-let magicans :string[]=["Harry Houdini", "David Blaine", "Doug Henning","Dai Vernon"];
-function show_magicans(magicansArray:string[]):void{
-    console.log("Magicans:")
-    magicansArray.forEach(magicans =>{
-        console.log(magicans)
-    });
-}
-function make_great(magicansArray:string[]): string[]{
-    return magicansArray.map(magicans => `Great ${magicans}`)
-};
-show_magicans(magicans);
- let greatMagicans=make_great([...magicans]);
-show_magicans(greatMagicans);
+function showMagican(magicans:string[]){
+     return magicans.forEach(orginal => console.log(orginal));
+ };
 
+ function makeGreat(magicans:string[]){
+     return magicans.map( (original) =>
+         `Great ${original}`)}
+
+
+ let magicansNames :string[] = ["Harry Houdini", "David Blaine", "Doug Henning","Dai Vernon"];
+ let greatMagicans :string[]= makeGreat([...magicansNames])
+
+ console.log("Original List:");
+ showMagican(magicansNames)
+
+ console.log("Modified List:");
+showMagican(greatMagicans);
